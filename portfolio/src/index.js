34fdcebc235676067727d1e3f2ef92c668816d22
Kevin Lawrence <kevin.lawrence404@gmail.com>
2022-07-13 19:44:48 -0700

    fixed site

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -6,12 +6,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import reportWebVitals from "./reportWebVitals";
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
